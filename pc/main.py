16a41521c5b76e069ef7d6ced9e5ee1f7d57d308
Greg Smith <smrq@smrq.net>
2022-06-16 14:29:45 -0500

    2

--- pc/main.py
+++ pc/main.py
@@ -2,16 +2,96 @@ import mido
 import struct
 import serial
 
-def serialize_analog(out, value):
-	return struct.pack('<cBf', 'a', out, value)
+def serialize(commands):
+	return b''.join(serialize_one(cmd) for cmd in commands) + b'\xFF'
 
-def serialize_digital(out value):
-	return struct.pack('<cB?', 'd', out, value)
+def serialize_one(command):
+	if command['type'] == 'analog':
+		return struct.pack('<cBf', b'a', command['pin'], command['value'])
+	if command['type'] == 'digital':
+		return struct.pack('<cB?', b'd', command['pin'], command['value'])
+	raise ValueError('invalid command type')
 
-with serial.Serial('/dev/ttyACM0', baudrate=1000000) as serial_out:
-	with mido.open_input() as midi_in:
-		for msg in midi_in:
+def midi_note_to_cv(note):
+	return note * (1/12)
+
+def open_serial_with_fallback(tty_name):
+	try:
+		return serial.Serial(tty_name, baudrate=1000000)
+	except serial.serialutil.SerialException as err:
+		print(err)
+		print('Running in disconnected mode.')
+		return SerialShim(tty_name)
+
+class SerialShim:
+	def __init__(self, tty_name):
+		self.tty_name = tty_name
+
+	def write(self, msg):
+		print('{} << {}'.format(self.tty_name, msg.hex(' ')))
+
+	def close(self):
+		pass
+
+class MidiCV:
+	def __init__(self, tty_name):
+		self.tty_name = tty_name
+		self.channels = [
+			{ 'note_pin': 12, 'gate_pin': 13, 'notes': [] },
+			{ 'note_pin': 14, 'gate_pin': 15, 'notes': [] },
+		]
+		self.poly_mode = 'last'
+
+	def __enter__(self):
+		self.midi_in = mido.open_input()
+		self.serial_out = open_serial_with_fallback(self.tty_name)
+		return self
+
+	def __exit__(self, *args, **kwargs):
+		self.midi_in.close()
+		self.serial_out.close()
+
+	def listen(self):
+		for msg in self.midi_in:
 			print(msg)
-			serialized = serialize_digital(msg.channel, msg.type == 'note_on')
-			print(serialized)
-			serial_out.write(serialized)
+
+			if msg.type == 'note_on':
+				try:
+					channel = self.channels[msg.channel]
+				except IndexError:
+					continue
+
+				channel['notes'].append(msg.note)
+				commands = [
+					{ 'type': 'analog', 'pin': channel['note_pin'], 'value': midi_note_to_cv(msg.note) },
+					{ 'type': 'digital', 'pin': channel['gate_pin'], 'value': True },
+				]
+				self.serial_out.write(serialize(commands))
+
+			elif msg.type == 'note_off':
+				try:
+					channel = self.channels[msg.channel]
+				except IndexError:
+					continue
+
+				try:
+					channel['notes'].remove(msg.note)
+				except ValueError:
+					continue
+
+				if len(channel['notes']) == 0:
+					commands = [
+						{ 'type': 'digital', 'pin': channel['gate_pin'], 'value': False },
+					]
+					self.serial_out.write(serialize(commands))
+				else:
+					note = channel['notes'][-1]
+					commands = [
+						{ 'type': 'analog', 'pin': channel['note_pin'], 'value': midi_note_to_cv(note) },
+						{ 'type': 'digital', 'pin': channel['gate_pin'], 'value': True },
+					]
+					self.serial_out.write(serialize(commands))
+
+with MidiCV('/dev/ttyACM0') as midicv:
+	midicv.listen()
+
--- teensy/main.ino
+++ teensy/main.ino
@@ -1,31 +1,57 @@
+const int dac_pins[12] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+const int cs_pins[] = { 12, 13 };
+const int analog_pin = 14;
+
 void setup() {
-	pinMode(12, OUTPUT);
-	pinMode(13, OUTPUT);
-	pinMode(14, OUTPUT);
+	for (int i = 0; i < 12; ++i) {
+		pinMode(dac_pins[i], OUTPUT);
+	}
+	for (int i = 0; i < sizeof(cs_pins); ++i) {
+		pinMode(cs_pins[i], OUTPUT);
+	}
+	pinMode(analogPin, OUTPUT);
+
 	analogWriteResolution(12);
 
 	while (!Serial);
 }
 
+uint16_t float_to_uint12(float value, float min_value, float max_value) {
+	return (uint16_t)((value-min_value) / (max_value-min_value) * (1<<12));
+}
+
+void write_analog(int pin, float value) {
+	uint16_t value_bits = float_to_uint12(value, 0f, 3.3f);
+	if (pin == analog_pin) {
+		analogWrite(pin, value_bits);
+	} else {
+		digitalWrite(pin, LOW);
+		for (int i = 0; i < 12; ++i) {
+			digitalWrite(dac_pins[i], value_bits & (1<<i) ? HIGH : LOW);
+		}
+		digitalWrite(pin, HIGH);
+	}
+}
+
 void loop() {
 	if (Serial.available()) {
-		char type = Serial.read();
-		uint8_t port = Serial.read();
-
-		switch (type) {
-			case 'a': {
-				float value;
-				Serial.readBytes(&value, sizeof(value));
-				uint8_t pin = 14;
-				analogWrite(pin, (int)(value / 3.3f * (1<<12)));
-				break;
-			}
-			case 'd': {
-				bool value = Serial.readByte();
-				uint8_t pin = port == 0 ? 12 : 13;
-				digitalWrite(pin, value ? HIGH : LOW);
-				break;
+		char type;
+		while ((type = Serial.read()) != 0xFF) {
+			uint8_t pin = Serial.read();
+			switch (type) {
+				case 'a': {
+					float value;
+					Serial.readBytes(&value, sizeof(value));
+					write_analog(pin, value);
+					break;
+				}
+				case 'd': {
+					bool value = Serial.readByte();
+					digitalWrite(pin, value ? HIGH : LOW);
+					break;
+				}
 			}
 		}
 	}
 }
+
