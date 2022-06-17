a38527173601c1088baa670087bf93ef24c794e4
Greg Smith <smrq@smrq.net>
2022-06-16 21:14:05 -0500

    3

--- teensy/main.ino
+++ teensy/main.ino
@@ -1,18 +1,11 @@
-const int dac_pins[12] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-const int cs_pins[] = { 12, 13 };
-const int analog_pin = 14;
+#include <SPI.h>
+
+const uint8_t digital_out[] = { 3, 4, 5, 6, 7, 8 };
+const uint8_t analog_out_cs[] = { 0, 0, 1, 1, 2, 2 };
+const uint8_t analog_out_ab[] = { 0, 1, 0, 1, 0, 1 };
 
 void setup() {
-	for (int i = 0; i < 12; ++i) {
-		pinMode(dac_pins[i], OUTPUT);
-	}
-	for (int i = 0; i < sizeof(cs_pins); ++i) {
-		pinMode(cs_pins[i], OUTPUT);
-	}
-	pinMode(analogPin, OUTPUT);
-
-	analogWriteResolution(12);
-
+	SPI.begin();
 	while (!Serial);
 }
 
@@ -20,33 +13,34 @@ uint16_t float_to_uint12(float value, float min_value, float max_value) {
 	return (uint16_t)((value-min_value) / (max_value-min_value) * (1<<12));
 }
 
-void write_analog(int pin, float value) {
-	uint16_t value_bits = float_to_uint12(value, 0f, 3.3f);
-	if (pin == analog_pin) {
-		analogWrite(pin, value_bits);
-	} else {
-		digitalWrite(pin, LOW);
-		for (int i = 0; i < 12; ++i) {
-			digitalWrite(dac_pins[i], value_bits & (1<<i) ? HIGH : LOW);
-		}
-		digitalWrite(pin, HIGH);
-	}
+void write_dac(uint8_t cs, uint8_t ab, float value) {
+	uint16_t value_bits = float_to_uint12(value, 0f, 10f)
+		| (ab << 15) // DAC_A (0) or DAC_B (1)
+		| (1<<13)    // Gain = 1x
+		| (1<<12);   // Output enabled
+	SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
+	digitalWrite(cs, LOW);
+	SPI.transfer16(value_bits);
+	digitalWrite(cs, HIGH);
 }
 
 void loop() {
 	if (Serial.available()) {
 		char type;
 		while ((type = Serial.read()) != 0xFF) {
-			uint8_t pin = Serial.read();
+			uint8_t out = Serial.read();
 			switch (type) {
 				case 'a': {
 					float value;
 					Serial.readBytes(&value, sizeof(value));
-					write_analog(pin, value);
+					uint8_t cs = analog_out_cs[out];
+					uint8_t ab = analog_out_ab[out];
+					write_dac(cs, ab, value);
 					break;
 				}
 				case 'd': {
 					bool value = Serial.readByte();
+					uint8_t pin = digital_out[out];
 					digitalWrite(pin, value ? HIGH : LOW);
 					break;
 				}
